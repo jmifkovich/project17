c23e00f026cc12e2a06a4ad10c52c15d6892d8e2
justin <justin@bitlife.dev>
2022-08-15 10:44:26 -0400

    Initial commit

---
+++ src/main/java/com/example/project17/Project17Application.java
@@ -0,0 +1,13 @@
+package com.example.project17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project17Application.class, args);
+	}
+
+}
